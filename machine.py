7539de0588dff4f477db13269cc55d9293baea70
username29800 <piapia78490@gmail.com>
2023-06-06 10:16:35 +0900

    work on python script

--- machine.py
+++ machine.py
@@ -1,10 +1,10 @@
+# initalize variables
 a = [0, 0 ,0 ,0 ,0]
 for i in range(0,len(a)):
-    a[i] = input()
+    a[i] = int(input())
 # start loop
 while True:
     for i in range(0, len(a)):
         if a[i] > 255:
             a[i] = -1*a[i]
     print (a)
-    break
